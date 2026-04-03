87fcb7fcab10d768af9772b8a5d6937567d9541f
lanjs <jayce404@foxmail.com>
2026-04-03 10:00:33 +0800

    修改日志工具类的使用（但是相比之前的直接使用相对运行缓慢）

--- chapter03/03_data_structure-list.py
+++ chapter03/03_data_structure-list.py
@@ -3,3 +3,5 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from utils.logger import log
+
+log.info("This is a log message from 03_data_structure-list.py")
--- utils/logger.py
+++ utils/logger.py
@@ -3,33 +3,30 @@ import logging.config
 import os
 import sys
 
-# 自动获取项目根目录（utils 上一级）
+# 项目根目录
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-LOG_CONF_PATH = os.path.join(BASE_DIR, "logging.conf")
+LOG_CONF = os.path.join(BASE_DIR, "logging.conf")
+LOG_DIR = os.path.join(BASE_DIR, "logs")
 
-class Logger:
+# 自动创建 logs 文件夹
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+
+class LoggerUtil:
     _inited = False
 
-    @staticmethod
-    def get_logger():
-        # 自动获取调用者文件名做 logger 名称
+    @classmethod
+    def get_logger(cls):
+        # 获取调用者文件名
         frame = sys._getframe(1)
         filename = frame.f_code.co_filename
         logger_name = os.path.splitext(os.path.basename(filename))[0]
 
-        if not Logger._inited:
-            if os.path.exists(LOG_CONF_PATH):
-                logging.config.fileConfig(LOG_CONF_PATH)
-            else:
-                # 没有配置文件时用默认配置
-                logging.basicConfig(
-                    level=logging.INFO,
-                    format="%(asctime)s [%(name)s] %(levelname)s: %(message)s",
-                    datefmt="%Y-%m-%d %H:%M:%S"
-                )
-            Logger._inited = True
+        if not cls._inited:
+            logging.config.fileConfig(LOG_CONF)
+            cls._inited = True
 
         return logging.getLogger(logger_name)
 
-# 简化调用
-log = Logger.get_logger()
+# 全局单例 log
+log = LoggerUtil.get_logger()
